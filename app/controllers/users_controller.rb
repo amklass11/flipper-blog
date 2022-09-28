2447b00c51c4936c812040c76c9773e5c283cae9
Amk Lass <lassanaa14@gmail.com>
2022-09-28 08:44:27 +0100

    fix errors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
         @user = User.new
       end
       def index
-        @users = User.paginate(page: params[:page], per_page: 5)
+        @users = User.all
       end
       
       def edit
